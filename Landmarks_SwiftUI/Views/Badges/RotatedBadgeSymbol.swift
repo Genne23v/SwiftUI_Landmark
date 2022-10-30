33a14ada2730cd815467947e21d07023f6ef8070
Wonkeun No <zakk23v@hotmail.com>
2022-10-29 22:20:37 -0400

    Create badges

---
+++ Landmarks_SwiftUI/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks_SwiftUI
+//
+//  Created by Wonkeun No on 2022-10-29.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotateBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
