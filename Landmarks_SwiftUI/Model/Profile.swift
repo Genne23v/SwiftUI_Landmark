a016c0822f441c5cfe4b3303b73515d441302975
Wonkeun No <zakk23v@hotmail.com>
2022-11-02 11:05:02 -0400

    Implement user profile

---
+++ Landmarks_SwiftUI/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks_SwiftUI
+//
+//  Created by Wonkeun No on 2022-11-02.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
